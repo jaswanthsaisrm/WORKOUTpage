6700292830fe368b3f96beeffe571ef613bab348
jaswanthsaisrm <149457159+jaswanthsaisrm@users.noreply.github.com>
2024-04-20 15:54:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function displaywrapper() {
+    var display = document.getElementById('wrapper');
+    display.style.display = "flex"; 
+}
+
+function hidewrapper() {
+    var hide = document.getElementById('wrapper');
+    hide.style.display = "none"; 
+}
